e086e7a6753db4047cf52bb007e6ad4c16826fd7
Isadora Carvalho <100846791+isadoramcl@users.noreply.github.com>
2022-11-21 16:07:55 -0300

    feat: adicionado teste consulta

---
+++ tests/test_consulta.cpp
@@ -0,0 +1,44 @@
+#include "doctest"
+#include "Consulta.hpp"
+
+// test get and set data structures
+TEST_CASE("Teste get e set data")
+{
+	Consulta c;
+	c.setData("01/01/2020");
+	CHECK(c.getData() == "01/01/2020");
+
+	CHECK_THROWS(c.setData("01/01/20"));
+	CHECK_THROWS(c.setData("01/01/20200"));
+}
+
+// test get and set hora
+TEST_CASE("Teste get e set hora")
+{
+	Consulta c;
+	c.setHora("12:00");
+	CHECK(c.getHora() == "12:00");
+
+	CHECK_THROWS(c.setHora("12:00:00"));
+	CHECK_THROWS(c.setHora("12:0"));
+}
+
+// test get and set paciente
+TEST_CASE("Teste get e set paciente")
+{
+	Consulta c;
+	Paciente p;
+	p.setNome("João");
+	CHECK_NOTHROW(c.setPaciente(p));
+	CHECK(c.getPaciente().getNome() == "João");
+}
+
+// test get and set medico
+TEST_CASE("Teste get e set medico")
+{
+	Consulta c;
+	Medico m;
+	m.setNome("João");
+	CHECK_NOTHROW(c.setMedico(m));
+	CHECK(c.getMedico().getNome() == "João");
+}
